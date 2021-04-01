514b73340c37a25ae411d0120fec981d3219d0cd
Haroun Ach <harounacho21@gmail.com>
2021-04-01 10:47:08 +0100

    Test server

---
+++ server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+// server port
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(
+    `Server listening on port: ${PORT} visit http://localhost:${PORT}`
+  );
+});
