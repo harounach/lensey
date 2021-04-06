43df28811017913ec7a845a924a2eb5d8e1a9001
Haroun Ach <harounacho21@gmail.com>
2021-04-06 22:43:49 +0100

    Set up front end files

--- server.js
+++ server.js
@@ -2,30 +2,22 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-const expressLayouts = require("express-ejs-layouts");
-
 // server port
 const PORT = 3000;
 
-const indexRoute = require("./routes/index");
+const newsRoute = require("./routes/newsRoute");
 
 // Apply middlewares
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Set up the view engine
-app.set("view engine", "ejs");
-app.set("views", __dirname + "/views");
-app.set("layout", "layouts/layout");
-app.use(expressLayouts);
-
 // app.get("/", (req, res) => {
 //   res.send("Hello, World!");
 // });
 
 // Set up routes
-app.use("/", indexRoute);
+app.use("/", newsRoute);
 
 app.listen(process.env.PORT || PORT, () => {
   console.log(
