4717dd61c158991785458a87a5c7dd6335609632
Haroun Ach <harounacho21@gmail.com>
2021-04-07 10:27:16 +0100

    Types definition

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,27 @@
 window.addEventListener("DOMContentLoaded", () => {
+  /* Types definition */
+
+  /**
+   * Article
+   * @typedef {Object} Article
+   *
+   * @property {Source} source
+   * @property {string} author
+   * @property {string} title
+   * @property {string} description
+   * @property {string} url
+   * @property {string} urlToImage
+   * @property {string} publishedAt
+   * @property {string} content
+   */
+
+  /**
+   * Source
+   * @typedef {Object} Source
+   *
+   * @property {string} id
+   * @property {string} name
+   */
+
   console.log("Hello, World!");
 });
